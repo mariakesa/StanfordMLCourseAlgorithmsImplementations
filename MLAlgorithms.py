19be01a277ab80019e9aea5d76d4ab511a65f4c9
Maria Kesa <maria.kesa@gmail.com>
2020-11-13 15:15:22 +0100

    templates

---
+++ MLAlgorithms.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class LinearRegression():
+
+
+    def fit(X,y,fitting_method='gradient_descent'):
+        '''
+        X is the data matrix with dimensions (data points, features).
+
+        y is the target vector of matrix, with targets organized into columns.
+
+        There are two fitting methods: gradient_descent, recommended for large datasets
+        and analytical, which involves matrix inversion that can be used when the
+        data is relatively small.
+        '''
+
+        
